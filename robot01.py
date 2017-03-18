f392e44f3e6152b39cf401631ce8844b99b450fc
Dulwich College IT <dulwichcollegeit@gmail.com>
2017-03-18 11:54:09 +0000

    Initial Code

---
+++ robot01.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO 
+import time 
+
+GPIO.setmode(GPIO.BCM) 
+GPIO.setwarnings(False) 
+
+GPIO.setup(7, GPIO.OUT) 
+GPIO.setup(8, GPIO.OUT) 
+GPIO.setup(9, GPIO.OUT) 
+GPIO.setup(10, GPIO.OUT) 
+
+GPIO.output(7, 0) 
+GPIO.output(8, 0) 
+GPIO.output(9, 0) 
+GPIO.output(10, 0)
+
+# right motor forwards 
+GPIO.output(9, 0) 
+GPIO.output(10, 1) 
+# Left Motor
+GPIO.output(7, 0) 
+GPIO.output(8, 1) 
+
+time.sleep(1) 
+
+GPIO.cleanup()
